fa55c88e570c80334bea1d7c2ed26f6d30db623a
Jens Reidel <adrian@travitia.xyz>
2024-07-31 18:24:52 +0200

    redwood: Assert for redwood or redwoodin
    Change-Id: I5b0ea72b0fd6f9b57ae57ac3c5e58c4e596ab357
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,9 @@ DEVICE_PATH := device/xiaomi/redwood
 # Inherit from sm8350-common
 include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 
+# Assert
+TARGET_OTA_ASSERT_DEVICE := redwood,redwoodin
+
 # Board
 TARGET_BOOTLOADER_BOARD_NAME := redwood
 
