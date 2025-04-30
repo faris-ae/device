2d6bc69df22d981879ba5d6393659c7dbe7892f4
Joey Huab <jhuab@edukasyon.ph>
2025-04-30 17:30:43 +0900

    redwood: Move camera package name flag to select()

--- device.mk
+++ device.mk
@@ -9,6 +9,7 @@ $(call inherit-product, device/xiaomi/sm8350-common/common.mk)
 
 # Camera
 $(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
+$(call soong_config_set,camera,package_name,com.android.camera)
 
 # Init
 PRODUCT_COPY_FILES += \
