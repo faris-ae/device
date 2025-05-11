36621fb5ce893f92e14704044967068d4c17009d
Silesh K Nair <sileshn@gmail.com>
2025-05-11 05:54:06 +0530

    fixup! redwood: add back dolby support
    *needed after media codecs move to vendor

--- extract-files.py
+++ extract-files.py
@@ -57,10 +57,10 @@ blob_fixups: blob_fixups_user_type = {
     ('vendor/etc/camera/pureShot_parameter.xml', 'vendor/etc/camera/pureView_parameter.xml'): blob_fixup()
         .regex_replace(r'=(\d+)>', r'="\1">'),
     ('vendor/etc/media_codecs_lahaina.xml', 'vendor/etc/media_codecs_system_default_yupik.xml'): blob_fixup()
-        .regex_replace('.+media_codecs_(c2_audio|dolby_audio|google_audio|google_c2|google_telephony|vendor_audio).+\n', '')
+        .regex_replace('.+media_codecs_(c2_audio|google_audio|google_c2|google_telephony|vendor_audio).+\n', '')
         .regex_replace(r'(?s)(<Settings.*?>)',r'\1\n        <Domain name="telephony" enabled="true" />'),
     ('vendor/etc/media_codecs_yupik_v1.xml'): blob_fixup()
-        .regex_replace('.+media_codecs_(dolby_audio|with_dolby_yupik).+\n', ''),
+        .regex_replace('.+media_codecs_with_dolby_yupik.+\n', ''),
     ('vendor/etc/init/android.hardware.drm@1.3-service.widevine.rc', 'vendor/etc/init/vendor.qti.media.c2@1.0-service.rc'): blob_fixup()
         .regex_replace('writepid /dev/cpuset/foreground/tasks', 'task_profiles ProcessCapacityHigh'),
     ('vendor/etc/init/android.hardware.neuralnetworks@1.3-service-qti.rc'): blob_fixup()
