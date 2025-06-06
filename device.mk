8e2d80f6569e0afe745708d47cd3b21bcdd9cb32
johnmart19 <johnivan19999@gmail.com>
2025-06-07 02:43:16 +0300

    redwood: miuiCamera: Move Camera Extensions to system framework & add bootjar

--- device.mk
+++ device.mk
@@ -126,7 +126,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera Extensions permissions
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/camerax-vendor-extensions.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/camerax-vendor-extensions.xml
+    $(LOCAL_PATH)/configs/permissions/camerax-vendor-extensions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/camerax-vendor-extensions.xml
 
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
