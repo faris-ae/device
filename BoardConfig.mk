f50d57c6c87dfe1a83fbc5884223fee21cfed363
Giovanni Ricca <giovanniricca@proton.me>
2025-06-27 19:01:12 +0200

    redwood: Load charger modules on recovery
    Depends-On: I835fa5daaa53eb704cc6c0777d7069d47ecbe10c
    Change-Id: Ib6ffccc2d21367d398e7ea57922798005ac6f9bd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -124,9 +124,17 @@ override KERNEL_TOOLCHAIN_PREFIX_arm := arm-linux-android-
 
 # Kernel modules
 BOOT_KERNEL_MODULES := \
+    adsp_loader_dlkm.ko \
+    apr_dlkm.ko \
     focaltech_touch.ko \
     goodix_core.ko \
+    mmhardware_sysfs_dlkm.ko \
+    q6_notifier_dlkm.ko \
+    q6_pdr_dlkm.ko \
+    qti_battery_charger_main.ko \
+    snd_event_dlkm.ko \
     xiaomi_touch.ko
+
 BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(BOOT_KERNEL_MODULES)
 
 # Partitions
