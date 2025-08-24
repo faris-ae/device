77f9fa21cf4cb7a8b78f31e46560e7e919c84be4
Thereache <thereache2003@gmail.com>
2025-08-24 12:43:40 +0530

    Revert "redwood: Remove vendor/lineage device framework matrix inclusion"
    This reverts commit 84924c912183b08f9f22229fa8783e7a133fe011.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,7 +79,8 @@ DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/hidl/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/hidl/manifest_lahaina.xml \
