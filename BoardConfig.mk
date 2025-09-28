40cf079f22d099493bc13802a261e00249461bbd
faris-ae <su.faris.ae@gmail.com>
2025-09-28 19:46:19 +0000

    redwood: update blobs from OS2.0.10.0.UMSMIXM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -200,7 +200,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2025-07-01
+VENDOR_SECURITY_PATCH := 2025-09-25
 
 # Sepolicy
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
